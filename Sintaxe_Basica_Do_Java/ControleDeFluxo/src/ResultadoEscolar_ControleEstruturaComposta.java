ecdb1bf8cc63c5bc8c303c96324842638b460f5c
lucas de souza santos <lucass.santoss210@gmail.com>
2025-02-05 17:28:29 +0000

    final da aula de condicionais composta

---
+++ Sintaxe_Basica_Do_Java/ControleDeFluxo/src/ResultadoEscolar_ControleEstruturaComposta.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolar_ControleEstruturaComposta {
+    public static void main(String[] args) {
+        System.out.println("Estrutura de Controle Composta");
+
+        double nota = 6.0;
+
+        if (nota >= 7)
+        {
+            System.out.println("Aprovado");
+        }
+        else
+        {
+            System.out.println("Reprovado");
+        }
+    }
+}
