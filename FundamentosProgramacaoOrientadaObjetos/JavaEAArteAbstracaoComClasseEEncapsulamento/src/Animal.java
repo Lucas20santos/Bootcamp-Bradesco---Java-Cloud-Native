8cf5cf4c0290a96d8f4f4041fcd46b64c9f7e815
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-03-16 01:01:59 +0000

    testando a criação de um objeto.

--- FundamentosProgramacaoOrientadaObjetos/JavaEAArteAbstracaoComClasseEEncapsulamento/src/Animal.java
+++ FundamentosProgramacaoOrientadaObjetos/JavaEAArteAbstracaoComClasseEEncapsulamento/src/Animal.java
@@ -1,3 +1,22 @@
-public class Animal {
+import petshop.Banho;
 
+public class Animal 
+{
+    public static void main(String[] args) 
+    {
+        while(true)
+        {
+            Banho banho = new Banho();
+            System.out.println("Deseja ir para o menu interativo[s/n]: ");
+            String opcao = System.console().readLine().toLowerCase();
+            if(opcao.equals("s"))
+            {
+                banho.menuPetShop();
+            }
+            else
+            {
+                break;
+            }
+        }        
+    }
 }
