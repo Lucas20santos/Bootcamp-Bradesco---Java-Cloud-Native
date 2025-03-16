1b7181a24d961d2ee24a04e8e2107b1739894a3b
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-03-16 00:55:47 +0000

    criação do menu que irá fazer interface com o usuario e deixar métodos e atributos privados para que o usuario não tenha contato direto.

--- FundamentosProgramacaoOrientadaObjetos/JavaEAArteAbstracaoComClasseEEncapsulamento/src/petshop/Banho.java
+++ FundamentosProgramacaoOrientadaObjetos/JavaEAArteAbstracaoComClasseEEncapsulamento/src/petshop/Banho.java
@@ -114,4 +114,58 @@ public class Banho
     {
         this.limparMaquina = limparMaquina;
     }
+
+    public void menuPetShop()
+    {
+        System.out.println("1 - Banho");
+        System.out.println("2 - Abasteacer com Água");
+        System.out.println("3 - Abastecer com Shampoo");
+        System.out.println("4 - Verificar nível de Água");
+        System.out.println("5 - Verificar nível de Shampoo");
+        System.out.println("6 - Verificar se tem Pet na Máquina");
+        System.out.println("7 - Colocar Pet na Máquina");
+        System.out.println("8 - Retirar Pet da Máquina");
+        System.out.println("9 - Limpar Máquina");
+        System.out.println("10 - Sair");
+
+        System.out.print("Digite a opção desejada: ");
+        int opcao = Integer.parseInt(System.console().readLine());
+        switch (opcao)
+        {
+            case 1:
+                System.out.println("Banho");
+                break;
+            case 2:
+                System.out.println("Abastecer com Água");
+                break;
+            case 3:
+                System.out.println("Abastecer com Shampoo");
+                break;
+            case 4:
+                System.out.println("Verificar nível de Água");
+                break;
+            case 5:
+                System.out.println("Verificar nível de Shampoo");
+                break;
+            case 6:
+                System.out.println("Verificar se tem Pet na Máquina");
+                break;
+            case 7:
+                System.out.println("Colocar Pet na Máquina");
+                break;
+            case 8:
+                System.out.println("Retirar Pet da Máquina");
+                break;
+            case 9:
+                System.out.println("Limpar Máquina");
+                break;
+            case 10:
+                System.out.println("Sair");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
+    }
 }
